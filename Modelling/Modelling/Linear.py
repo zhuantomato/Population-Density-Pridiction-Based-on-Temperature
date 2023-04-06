c0cafa4664851f0bc5415e1e341609914243fe34
Ruijie Ni <1924975712@qq.com>
2023-04-06 12:02:56 +0800

    1st gen deep learning methods

---
+++ Modelling/Modelling/Linear.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# 读取数据文件
+data = pd.read_csv('/content/drive/MyDrive/FinalProject/Population-Density-Pridiction-Based-on-Temperature/Modelling/DataPreprocess/Weather/data.csv')
+
+# 将数据分为输入和输出
+X = data[['temperature', 'is_weekend', 'hour']]
+y = data['status']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# 训练线性回归模型
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# 预测结果
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+
+
+# 绘制图像
+plt.scatter(y_test, y_pred)
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+plt.xlim(1,2.5)
+plt.ylim(1,2.5)
+plt.text(0.95, 0.95, 'MSE: {:.4f}'.format(mse), transform=plt.gca().transAxes)
+plt.savefig('/content/drive/MyDrive/FinalProject/Population-Density-Pridiction-Based-on-Temperature/Modelling/Results/LinearSimpleScatter.png')
